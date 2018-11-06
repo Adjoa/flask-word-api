9664178e1cc515a74e5424dbd234c35763e6013c
adjoadarien <adjoa.darien@gmail.com>
2018-11-06 18:36:15 -0500

    test /words/rhyming exists

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,9 +10,17 @@ class TestApp(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
         
-    def test_wordsapi_creation(self):
-        res = self.client().get('/words')
-        self.assertEqual(res.status_code, 200)
+    # def test_wordsapi_creation(self):
+    #     res = self.client().get('/words')
+    #     self.assertEqual(res.status_code, 200)
+    
+    def test_words_random_endpoint_exists(self):
+        res = self.client().get('/words/random')
+        self.assertEqual(res)
+
+    def test_words_rhyming_endpoint_exists(self):
+        res = self.client().get('/words/rhyming')
+        self.assertEqual(res)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
