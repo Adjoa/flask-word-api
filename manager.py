1f107cb06670767c0d45d6e7a2f3ecf84874a45f
adjoadarien <adjoa.darien@gmail.com>
2018-11-06 15:17:37 -0500

    added manage.py

---
+++ manager.py
@@ -0,0 +1,9 @@
+import os
+from flask_script import Manager # class for handling a set of commands
+from app import create_app
+
+app = create_app(config_name=os.getenv('APP_SETTINGS'))
+manager = Manager(app)
+
+if __name__ == '__main__':
+    manager.run()
