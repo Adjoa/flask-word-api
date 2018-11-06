4c7a9f63b58f9c3db7000d831e3ca7b443bdde3a
adjoadarien <adjoa.darien@gmail.com>
2018-11-06 15:15:00 -0500

    added app/__init.py

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask_api import FlaskAPI
+
+# local import
+from instance.config import app_config
+from flask import request, jsonify, abort
+
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    @app.route('/words', methods=['GET'])
+    def words():
+        if request.method == "GET":
+            response.status_code = 200
+            return response
+
+    return app
